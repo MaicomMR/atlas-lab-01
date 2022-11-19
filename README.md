f31f98ebadd1941e76483320ac544822ea40da32
Maicom M. Rodeghiero <maicom_mr@hotmail.com>
2022-11-18 23:23:30 -0300

    chore: Update docker build files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# instant-docker
+
+# Como iniciar o projeto
+
+
+1. Buildar os containers
+`docker-compose up -d`
+
+2. Dar permissão para executar o script de inicialização
+`sudo chmod -R 777 start.sh`
+
+3. Rodar o script para instalar as dependências e migrações do banco
+`./start.sh`
--- start.sh
+++ start.sh
@@ -6,19 +6,19 @@ echo "\e[1;42m ■ Creating .env \e[0m"
 cp src/.env.example src/.env || echo "\e[1;41m FAIL \e[0m"
 
 echo "\e[1;42m ■ Installing back-end dependencies \e[0m"
-docker exec -it app composer install
+docker exec -it atlas-lab composer install
 
 echo "\e[1;42m ■ Artisan key:generate \e[0m"
-docker exec -it app php artisan key:generate || echo "\e[1;41m FAIL \e[0m"
+docker exec -it atlas-lab php artisan key:generate || echo "\e[1;41m FAIL \e[0m"
 
 echo "\e[1;42m ■ Migrate database \e[0m"
-docker exec -it app php artisan migrate || echo "\e[1;41m FAIL \e[0m"
+docker exec -it atlas-lab php artisan migrate || echo "\e[1;41m FAIL \e[0m"
 
 echo "\e[1;42m ■ Installing front-end dependencies \e[0m"
-docker exec -it app npm install
+docker exec -it atlas-lab npm install
 
 echo "\e[1;42m ■ Compile frontend files \e[0m"
-docker exec -it app npm run dev || echo "\e[1;41m FAIL \e[0m"
+docker exec -it atlas-lab npm run dev || echo "\e[1;41m FAIL \e[0m"
 
 echo ""
 echo "\e[1;42m ╔═════════════════╗ \e[0m"
