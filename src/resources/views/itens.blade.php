6af645af08e3aa28d1e75b628fe9d90a40d699f1
Augusto Pedroso <83038881+pedroso36@users.noreply.github.com>
2022-11-26 12:18:15 -0300

    Delete Itens

--- src/resources/views/itens.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-@foreach($compras as $compra)
-    <p style="display: inline-block; color: blue" >{{$compra->produto}}</p> || <p style="display: inline-block; color: red">{{$compra->valor}}</p>
-@endforeach
-</html>
